6adc18a22dff76e40255f81513ca25b411c33472
tom-256 <t11a.256.512.1024@gmail.com>
2019-12-16 22:45:44 +0900

    fix invalid href passed to router

--- pages/index.js
+++ pages/index.js
@@ -48,24 +48,18 @@ const Home = () => {
           )}
         </div>
         <div className='row'>
-          <Link href='https://github.com/zeit/next.js#setup'>
-            <a className='card'>
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-            <a className='card'>
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href='https://github.com/zeit/next.js'>
-            <a className='card'>
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
+          <a className='card' href='https://github.com/zeit/next.js#setup'>
+            <h3>Getting Started &rarr;</h3>
+            <p>Learn more about Next.js on GitHub and in their examples.</p>
+          </a>
+          <a className='card' href='https://github.com/zeit/next.js/tree/master/examples'>
+            <h3>Examples &rarr;</h3>
+            <p>Find other example boilerplates on the Next.js GitHub.</p>
+          </a>
+          <a className='card' href='https://github.com/zeit/next.js'>
+            <h3>Create Next App &rarr;</h3>
+            <p>Was this tool helpful? Let us know how we can improve it!</p>
+          </a>
         </div>
       </div>
 
